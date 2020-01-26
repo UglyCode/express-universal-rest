8889f3e55772b188d0425d0db90a813ccf35e811
uglyCode <appostnikov@gmail.com>
2020-01-26 23:15:52 +0300

    add table post handler

---
+++ controllers/postgres.js
@@ -0,0 +1,68 @@
+const {Client} = require('pg');
+const DATABASE_LINK = process.env.DATABASE_URL ||
+    'postgres://postgres:kloop12@localhost:5432/postgres';
+
+const client = new Client({
+    connectionString: DATABASE_LINK,
+});
+client.connect()
+    .catch(e => handleLocalError(e));
+
+const handleTableGet = (req,res) => {
+    if (!req.params.table_name){
+        noTableNameHandler(res);
+        return;
+    }
+    const table = req.params.table_name;
+
+    client.query(`SELECT * FROM public.${table};`)
+        .then(result => sendQueryResult(result, res))
+        .catch(e => handleLocalError(e,res));
+};
+
+const handleTablePost = (req, res) => {
+    if (!req.params.table_name){
+        noTableNameHandler(res);
+        return;
+    }
+    const table = req.params.table_name;
+    const payload = req.body;
+
+    client.query(
+        `INSERT INTO public.${table} (${Object.keys(payload[0]).join(',')})
+        VALUES ${payload.reduce((accum, elem, i, arr) => {
+        accum += `(${Object.values(elem).join(',')})` + (arr.length-1 === i) ? ' ':', ';
+        return accum;
+        }, '')};`
+    )
+        .then(result => sendQueryResult(result, res))
+        .catch(e => handleLocalError(e,res));
+
+};
+
+
+const sendQueryResult = (queryResult, res) => {
+
+    let resBody = queryResult.rows;
+
+    if (!resBody || !Array.isArray(resBody)){
+        res.status(500);
+        resBody = 'Invalid query result';
+    } else if (resBody.length === 0){
+        resBody = 'Table is empty';
+    }
+
+    res.json(resBody);
+};
+
+const noTableNameHandler = (res) => {
+    res.status(400).json('no table name passed.');
+};
+
+const handleLocalError = (e, res) => {
+    console.log('--error: ' + e);
+    console.log('-- at: ' + e.stack);
+    res.status(500).json(e.stack);
+};
+
+module.exports = {handleTableGet, handleTablePost};
--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const bp = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const pgHandlers = require('./controllers/postgres');
+
 const app = express();
 app.use(cors());
 app.use(morgan('tiny'));
 app.use('/static',express.static(__dirname + '/public'));
 app.use(bp.json());
 
-app.get('/:table_name', (req, res)=>{
-
-});
+app.get('/:table_name', (req, res) => pgHandlers.handleTableGet(req, res));
 
 app.post('/:table_name', (req, res)=>{
 
