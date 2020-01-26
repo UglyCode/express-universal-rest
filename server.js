d71cdc045c514ff61f832e903404b179cad08d2e
uglyCode <appostnikov@gmail.com>
2020-01-26 21:32:36 +0300

    initial update

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bp = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const DATABASE_LINK = process.env.DATABASE_URL || 'localhost:5432';
+
+const app = express();
+app.use(cors());
+app.use(morgan('tiny'));
+app.use('/static',express.static(__dirname + '/public'));
+app.use(bp.json());
+
+app.get('/', (req, res)=>{
+
+});
+
+app.post('/', (req, res)=>{
+
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, ()=>{
+   console.log(`server started at port: ${PORT}`);
+});
